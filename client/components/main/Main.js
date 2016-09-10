42e75c134a225e399217d1c49c9846d4a8b0006d
Taylor Page <jonathantaylorpage@gmail.com>
2016-09-09 20:46:28 -0500

    (feat) Add navigation bar

--- client/components/main/Main.js
+++ client/components/main/Main.js
@@ -9,8 +9,16 @@ export default class Main extends React.Component {
   render() {
     return (
       <div>
-        <Nav />
-        <h1 className="col-xs-12 title">The Radio Galaxy</h1>
+        <div className="container-fluid">
+          <Nav />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <h1 className="title">The Radio Galaxy</h1>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- client/components/nav/Nav.js
+++ client/components/nav/Nav.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default class Nav extends React.Component {
   render() {
     return (
-      <div className="col-xs-12 nav">
-        <a className="link" href="#">Sign In</a>
-        <a className="link" href="#">Sign Up</a>
+      <div className="row">
+        <div className="col-md-12 text-md-right navbar">
+          <a className="col-md-2 text-md-left white logo" href="#">The Radio Galaxy</a>
+          <a className="col-md-1 offset-md-8 white" href="#">Sign In</a>
+          <a className="col-md-1 white" href="#">Sign Up</a>
+        </div>
       </div>
     )
   }
