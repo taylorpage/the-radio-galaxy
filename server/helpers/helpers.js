4a4ffa7cd765d7f7c2a980de80b1ce3efea4bae6
Taylor Page <jonathantaylorpage@gmail.com>
2016-09-11 17:23:46 -0500

    (chore) Create search function and server route

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,3 +1,11 @@
 const request = require('request');
+const apikey = require('../config/config');
 /* All Helper Functions Belong Here
    Use callbacks to return values back to the routes */
+
+exports.bandsInTown = function(query, cb) {
+  var url = `http://api.bandsintown.com/artists/${query}/events.json?api_version=2.0&app_id=theradiogalaxy`
+  request(url, function(err, res) {
+    cb(res.body);
+  });
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,13 @@
 const helpers = require('../helpers/helpers');
 
 module.exports = function(app, express) {
+  app.post('/concertSearch', function(req, res) {
+    var genre = req.body.genre;
+    var query = req.body.query;
+    var latlong = `${req.body.lat},${req.body.long}`;
 
+    helpers.bandsInTown(query, function(data) {
+      res.status(200).send(data);
+    });
+  })
 }
