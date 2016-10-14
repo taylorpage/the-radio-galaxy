b12e7a8f4dcd1f68763bebf9673f7592164c7951
Taylor Page <jonathantaylorpage@gmail.com>
2016-10-14 08:48:39 -0500

    (feat) Ability to sign in and sign out

--- client/components/nav/Nav.js
+++ client/components/nav/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const path = 'http://127.0.0.1:3000'
+
 export default class Nav extends React.Component {
   constructor(props) {
     super(props)
@@ -16,12 +18,24 @@ export default class Nav extends React.Component {
     }
   }
 
+  toggle_login() {
+    if (this.state.logged_in) {
+      sessionStorage.clear();
+      this.setState({
+        logged_in: false
+      })
+    } else {
+      window.location = `${path}/login`
+    }
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-md-12 navbar">
           <a className="col-md-2 text-md-left white space" href="#">The Radio Galaxy</a>
-          <a className="col-md-1 offset-md-8 white" href="#"> { 
+          <a className="col-md-1 offset-md-8 white"
+             onClick={ this.toggle_login.bind(this) }> { 
             this.state.logged_in ? 'Sign Out' : 'Sign In'
           }</a>
           <a className="col-md-1 white" href="#">Sign Up</a>
