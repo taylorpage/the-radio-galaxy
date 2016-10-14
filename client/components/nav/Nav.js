3b87d12c1e090a15fe77e4a3e7baaaa446addb55
Taylor Page <jonathantaylorpage@gmail.com>
2016-10-14 08:40:17 -0500

    (chore) Display sign in and sign out depending on login status

--- client/components/nav/Nav.js
+++ client/components/nav/Nav.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 export default class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      logged_in: false
+    }
+  }
+
+  componentWillMount() {
+    if (sessionStorage.getItem('user_email')) {
+      this.setState({
+        logged_in: true
+      })
+    }
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-md-12 navbar">
           <a className="col-md-2 text-md-left white space" href="#">The Radio Galaxy</a>
-          <a className="col-md-1 offset-md-8 white" href="#">Sign In</a>
+          <a className="col-md-1 offset-md-8 white" href="#"> { 
+            this.state.logged_in ? 'Sign Out' : 'Sign In'
+          }</a>
           <a className="col-md-1 white" href="#">Sign Up</a>
         </div>
       </div>
